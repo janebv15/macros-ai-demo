5d2c84ff5f635ca291e8291407374591b8179dc2
janebv15 <jvaughn0@chicagobooth.edu>
2025-04-28 20:02:03 +0000

    Updates

--- app/controllers/Macros_controller.rb
+++ app/controllers/Macros_controller.rb
@@ -1,6 +1,15 @@
 class Macros_ControllerClass < ApplicationController
 
+
+  
     def display_form
       render ({ :template => "macro_templates/a_form"})
     end
-    
+
+    def process_inputs
+      @the_image = params.fetch("image_param")
+      @the_description = params.fetch("description_param")
+  
+      render({ :template => "macro_templates/results" })
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
 
 
+
+  get("/blank_form", { :controller => "macros", :action => "display_form" })
+ 
+  get("/process_form_inputs", { :controller => "macros", :action => "process_inputs" })
+
   # Solutions below. Don't peek until you try it yourself and get stuck!
 
   get("/solutions/blank_form", { :controller => "solutions", :action => "display_form" })
-  
+
   post("/solutions/process_form", { :controller => "solutions", :action => "process_inputs" })
-  
+
 end
